d64bf7cf8e937214c0e64bd6230a71268b997b1b
wanganyu <2508635478@qq.com>
2018-07-31 23:21:16 +0800

    修复BUG

--- src/main/java/com/baichuan/concurrency/example/commonUnsafe/ArrayListExample.java
+++ src/main/java/com/baichuan/concurrency/example/commonUnsafe/ArrayListExample.java
@@ -1,6 +1,7 @@
 package com.baichuan.concurrency.example.commonUnsafe;
 
-import com.mmall.concurrency.annoations.NotThreadSafe;
+
+import com.baichuan.concurrency.annoations.NotThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
